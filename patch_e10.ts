fbbea831fc118ff1aa9dde33f1fbc1858d153c98
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-07-05 19:12:17 +0600

    added patch for error 10

---
+++ patch_e10.ts
@@ -0,0 +1,30 @@
+Java.perform(() => {
+    const scriptName = "patch_e10.js"
+    console.log(`[${scriptName}] injecting`);
+    try {
+        const inputStream = Java.use("java.io.InputStream");
+        inputStream.read.overload('[B').implementation = function(buffer: number[]) {
+            let bufferIndex = 0;
+            // patch only PipedInputStream
+            if (this.toString().includes("Piped")) {
+                let readResult = 0;
+                while (bufferIndex < buffer.length) {
+                    readResult = this.read();
+                    if (readResult >= 0) {
+                        buffer[bufferIndex] = readResult;
+                        bufferIndex += 1;
+                    } else {
+                        break;
+                    }
+                }
+                console.log(`[${scriptName}] ${this} :: read(byte[]) = ${bufferIndex}`)
+            } else {
+                bufferIndex = this.read(buffer);
+            }
+            return bufferIndex;
+        }
+
+    } catch (e) {
+        console.log(e);
+    }
+});
--- sign.py
+++ sign.py
@@ -30,7 +30,9 @@ def run_just_list(package):
     input()
 
 
-async def run_just_signature(package, relaunch):
+async def run_just_signature(package, **kwargs):
+    relaunch = kwargs.get("relaunch", False)
+
     device = frida.get_usb_device()
 
     app = get_app(device, package, relaunch)
@@ -39,11 +41,16 @@ async def run_just_signature(package, relaunch):
     print(f"[sign.py] extracted signature: {payload['signatureSha']}")
 
 
-def run_with_signature(package, signature, relaunch):
+def run_with_signature(package, signature, **kwargs):
+    relaunch = kwargs.get("relaunch", False)
+    patche10 = kwargs.get("patche10", False)
+
     device = frida.get_usb_device()
 
     gms = FridaAsync.attach(device, "com.google.android.gms.persistent")
     allow = gms.inject("allow.js")
+    if patche10:
+        gms.inject("patch_e10.js")
 
     payload = {"packageName": package,
                "signatureSha": signature}
@@ -56,11 +63,16 @@ def run_with_signature(package, signature, relaunch):
     input()
 
 
-async def run_auto_signature(package, relaunch):
+async def run_auto_signature(package, **kwargs):
+    relaunch = kwargs.get("relaunch", False)
+    patche10 = kwargs.get("patche10", False)
+
     device = frida.get_usb_device()
 
     gms = FridaAsync.attach(device, "com.google.android.gms.persistent")
     allow = gms.inject("allow.js")
+    if patche10:
+        gms.inject("patch_e10.js")
 
     app = get_app(device, package, relaunch)
 
@@ -77,15 +89,17 @@ async def run_auto_signature(package, relaunch):
 @click.option("-g", "--get-signature", "just_signature", is_flag=True, help="Get SHA-256 of the app signature")
 @click.option("-a", "--allowed-packages", "just_allowed", is_flag=True, help="List all allowed packages")
 @click.option("-r", "--relaunch", "relaunch", is_flag=True, help="Force re-launch of the app")
-def main(package, signature, just_signature, just_allowed, relaunch):
+@click.option("-e", "--patch-e10", "patche10", is_flag=True, help="Patch bug in Play Services causing error 10")
+def main(package, signature, just_signature, just_allowed, relaunch, patche10):
+    kwargs = {"relaunch": relaunch, "patche10": patche10}
     if just_signature:
-        asyncio.run(run_just_signature(package, relaunch))
+        asyncio.run(run_just_signature(package, **kwargs))
     elif just_allowed:
         run_just_list(package)
     elif signature:
-        run_with_signature(package, signature, relaunch)
+        run_with_signature(package, signature, **kwargs)
     else:
-        asyncio.run(run_auto_signature(package, relaunch))
+        asyncio.run(run_auto_signature(package, **kwargs))
 
 
 if __name__ == "__main__":
