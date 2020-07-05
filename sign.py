d78eaea70435d3641525c62343b53a1820ea10ed
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-07-05 18:05:19 +0600

    if app is launched - auto connect

--- sign.py
+++ sign.py
@@ -4,29 +4,42 @@ import frida
 from FridaAsync import FridaAsync
 
 
+def launch_app(device, package):
+    app = FridaAsync.spawn(device, package)
+    app.resume()
+    return app
+
+
+def get_app(device, package, relaunch):
+    if relaunch:
+        return launch_app(device, package)
+    try:
+        return FridaAsync.attach(device, package)
+    except frida.ProcessNotFoundError:
+        return launch_app(device, package)
+
+
 def run_just_list(package):
     device = frida.get_usb_device()
 
     gms = FridaAsync.attach(device, "com.google.android.gms.persistent")
     gms.inject("allow.js")
 
-    app = FridaAsync.spawn(device, package)
-    app.resume()
+    launch_app(device, package)
 
     input()
 
 
-async def run_just_signature(package):
+async def run_just_signature(package, relaunch):
     device = frida.get_usb_device()
 
-    app = FridaAsync.spawn(device, package)
-    app.resume()
+    app = get_app(device, package, relaunch)
 
     payload = await app.inject_async("signature.js")
     print(f"[sign.py] extracted signature: {payload['signatureSha']}")
 
 
-def run_just_patch(package, signature):
+def run_with_signature(package, signature, relaunch):
     device = frida.get_usb_device()
 
     gms = FridaAsync.attach(device, "com.google.android.gms.persistent")
@@ -37,36 +50,19 @@ def run_just_patch(package, signature):
 
     print(f"[sign.py] providing payload: {payload}")
     allow.post({"type": "signature", "payload": payload})
+    
+    get_app(device, package, relaunch)
 
     input()
 
 
-def run_with_signature(package, signature):
+async def run_auto_signature(package, relaunch):
     device = frida.get_usb_device()
 
     gms = FridaAsync.attach(device, "com.google.android.gms.persistent")
     allow = gms.inject("allow.js")
 
-    payload = {"packageName": package,
-               "signatureSha": signature}
-
-    print(f"[sign.py] providing payload: {payload}")
-    allow.post({"type": "signature", "payload": payload})
-
-    app = FridaAsync.spawn(device, package)
-    app.resume()
-
-    input()
-
-
-async def run_auto_signature(package):
-    device = frida.get_usb_device()
-
-    gms = FridaAsync.attach(device, "com.google.android.gms.persistent")
-    allow = gms.inject("allow.js")
-
-    app = FridaAsync.spawn(device, package)
-    app.resume()
+    app = get_app(device, package, relaunch)
 
     payload = await app.inject_async("signature.js")
     print(f"[sing.py] providing payload: {payload}")
@@ -80,21 +76,16 @@ async def run_auto_signature(package):
 @click.option("-s", "--signature", "signature", help="SHA-256 of the app signature (optional)")
 @click.option("-g", "--get-signature", "just_signature", is_flag=True, help="Get SHA-256 of the app signature")
 @click.option("-a", "--allowed-packages", "just_allowed", is_flag=True, help="List all allowed packages")
-@click.option("-j", "--just-patch", "just_patch", is_flag=True, help="Just apply patch to GMS, without launching the app (requires -s)")
-def main(package, signature, just_signature, just_allowed, just_patch):
-    if just_patch:
-        if not signature:
-            click.echo(f"\nError: just patch option requires signature. Get it with:\n\npipenv run python sign.py -p {package} -g")
-        else:
-            run_just_patch(package, signature)
-    elif just_signature:
-        asyncio.run(run_just_signature(package))
+@click.option("-r", "--relaunch", "relaunch", is_flag=True, help="Force re-launch of the app")
+def main(package, signature, just_signature, just_allowed, relaunch):
+    if just_signature:
+        asyncio.run(run_just_signature(package, relaunch))
     elif just_allowed:
         run_just_list(package)
     elif signature:
-        run_with_signature(package, signature)
+        run_with_signature(package, signature, relaunch)
     else:
-        asyncio.run(run_auto_signature(package))
+        asyncio.run(run_auto_signature(package, relaunch))
 
 
 if __name__ == "__main__":
