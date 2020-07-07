c82a4acba0e67b7bd46b0d0fe26f232458be2979
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-07-07 22:24:11 +0600

    moved frida wrapper inside en

--- en.py
+++ en.py
@@ -1,11 +1,32 @@
 import click
 import frida
-from FridaWrapper import FridaWrapper
+from frida.core import Session, Device, Script
 
 
 allow_script = "js/allow.js"
 signature_script = "js/signature.js"
 patche10_script = "js/patch_e10.js"
+
+
+class FridaWrapper:
+    def __init__(self, device: Device, session: Session):
+        self.device = device
+        self.session = session
+
+    @staticmethod
+    def attach(device: Device, app_name: str):
+        session: Session = device.attach(app_name)
+        return FridaWrapper(device, session)
+
+    def readjs(self, path: str) -> Script:
+        with open(path) as f:
+            code = f.read()
+        return self.session.create_script(code)
+
+    def inject(self, js_file: str) -> Script:
+        script: Script = self.readjs(js_file)
+        script.load()
+        return script
 
 
 @click.group()
