2ceacb2e41af5e52d305b4fa49009b5b8058702f
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-07-07 22:20:41 +0600

    refactoring click

--- en.py
+++ en.py
@@ -9,26 +9,11 @@ patche10_script = "js/patch_e10.js"
 
 
 @click.group()
-def list_allowed_group():
+def main():
     pass
 
 
-@click.group()
-def get_signature_group():
-    pass
-
-
-@click.group()
-def sign_group():
-    pass
-
-
-@click.group()
-def patch_only_group():
-    pass
-
-
-@list_allowed_group.command("list-allowed")
+@main.command("list-allowed")
 def list_allowed():
     """List all the app names allowed by Exposure Notifications"""
     device = frida.get_usb_device()
@@ -39,7 +24,7 @@ def list_allowed():
     input()
 
 
-@get_signature_group.command("get-signature")
+@main.command("get-signature")
 @click.option("-p", "--package", "package", help="Package name", required=True)
 def get_signature(package):
     """Get signature of the specified app"""
@@ -51,7 +36,6 @@ def get_signature(package):
 
 
 def sign(package, signature, patche10, forcedk, unlimiteddk):
-    """Allow the custom app to use Exposure Notifications"""
     device = frida.get_usb_device()
 
     gms = FridaWrapper.attach(device, "com.google.android.gms.persistent")
@@ -71,7 +55,7 @@ def sign(package, signature, patche10, forcedk, unlimiteddk):
     input()
 
 
-@sign_group.command("sign")
+@main.command("sign")
 @click.option("-p", "--package", "package", help="Package name (has to be one of the allowed apps)", required=True)
 @click.option("-s", "--signature", "signature", help="SHA-256 of the app signature", required=True)
 @click.option("-f", "--force-dk", "forcedk", is_flag=True, help="Force Diagnosis Keys signature validation")
@@ -82,17 +66,15 @@ def sign(package, signature, patche10, forcedk, unlimiteddk):
 @click.option("-e", "--patch-e10", "patche10", is_flag=True,
               help="Patch bug in Play Services causing error 10 (Pipe is closed, affects Android 6)")
 def sign_command(**kwargs):
+    """Allow the custom app to use Exposure Notifications"""
     sign(**kwargs)
 
 
-@patch_only_group.command("patch")
+@main.command("patch")
 def patch():
     """Patch a bug in Play Services affecting Android 6"""
     sign("dummy", "dummy", True, False, False)
 
 
-cli = click.CommandCollection(sources=[list_allowed_group, get_signature_group, sign_group, patch_only_group])
-
-
 if __name__ == "__main__":
-    cli()
+    main()
