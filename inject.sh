f597e2770a438cfd2a0c723072f7a547956de67a
Michael Huebler <michael@huebler.org>
2020-07-10 16:11:21 +0200

    Added -e to ps On my Android 9, ps without -e shows only sh and ps

--- inject.sh
+++ inject.sh
@@ -1,3 +1,4 @@
+#!/system/bin/sh
 fridaserver=/data/local/tmp/frida-server-12.8.12-android-arm
 fridainject=/data/local/tmp/frida-inject-12.8.12-android-arm
 allowscript=/data/local/tmp/allow.js
@@ -7,25 +8,25 @@ echo "[$scriptname] starting"
 
 while true
 do
-  fridarunning=`ps | grep frida-server | wc -l`
-  scriptrunning=`ps | grep frida-inject | wc -l`
+  fridarunning=`ps -e | grep frida-server | wc -l`
+  scriptrunning=`ps -e | grep frida-inject | wc -l`
   if [ $fridarunning -ne 1 ]
   then
     if [ $scriptrunning -eq 1 ]
     then
       echo "[$scriptname] killing inject"
-      ps | grep frida-inject | xargs sh -c 'kill -9 $1'
+      ps -e | grep frida-inject | xargs sh -c 'kill -9 $1'
     fi
     echo "[$scriptname] starting frida"
     $fridaserver -D
   fi
 
   sleep 10
-  scriptrunning=`ps | grep frida-inject | wc -l`
+  scriptrunning=`ps -e | grep frida-inject | wc -l`
   if [ $scriptrunning -ne 1 ]
   then
     echo "[$scriptname] starting inject"
-    $fridainject -n com.google.android.gms.persistent -s $allowscript --parameters '{"packageName":"de.rki.coronawarnapp.dev","signatureSha":"854528796DB85A3155FAAF92043CD3C42163CB9FA3C6709324A7F39DF4158462","forcedk":true,"unlimiteddk":true}' &
+    $fridainject -n com.google.android.gms.persistent -s $allowscript -P '{"packageName":"de.rki.coronawarnapp.dev","signatureSha":"854528796DB85A3155FAAF92043CD3C42163CB9FA3C6709324A7F39DF4158462","forcedk":"true","unlimiteddk":"true"}' &
   fi
   sleep 10
 done
